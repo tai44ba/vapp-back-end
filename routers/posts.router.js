c1cd7aafd8f654f35bc3c922ce9700f78d204446
Taishi <taishi.shibamoto@dci-student.org>
2024-08-27 13:52:56 +0200

    Stop tracking files in .gitignore

--- routers/posts.router.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import { getAllPosts } from '../controllers/posts.controller.js';
-
-const postRouter = Router()
-
-postRouter.route('/').post(getAllPosts)
-
-export default postRouter
